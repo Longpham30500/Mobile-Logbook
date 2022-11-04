b82461821c109cad7f87b5d83366bf42f066e2fb
Phạm Long <Longpham30500@gmail.com>
2022-11-04 19:26:26 +0700

    add form ex2 add Link

--- App.js
+++ App.js
@@ -1,22 +1,40 @@
-import { StyleSheet, Text, View , Image, Button, Alert} from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View , Image, Button, Alert, TextInput, TouchableOpacity} from 'react-native';
 
 export default function App() {
+  const [linkImg, setLinkImg] = useState('https://itechnolabs.ca/wp-content/uploads/2022/01/complete-guide-to-react-native-for-cross-platform-apps-development-itechnolabs.jpg')
   return (
     <View style={styles.container}>
       <Image
         style={styles.stretch}
-        source={{uri: "https://itechnolabs.ca/wp-content/uploads/2022/01/complete-guide-to-react-native-for-cross-platform-apps-development-itechnolabs.jpg"}}
+        source={{uri: linkImg}}
       />
       <View style={styles.fixToText}>
-        <Button
-          title="Backward"
-          onPress={() => Alert.alert('Backward button pressed')}
-        />
-        <Button
-          title="Forward"
-          onPress={() => Alert.alert('Forward button pressed')}
-        />
+
+        <TouchableOpacity onPress={() => Alert.alert('Backward button pressed')}
+        style ={styles.buttonCustom}>
+            <Text style={styles.text}>Backward</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => Alert.alert('Forward button pressed')}
+        style ={styles.buttonCustom}>
+            <Text style={styles.text}>Forward</Text>
+        </TouchableOpacity>
+
       </View>
+
+      <Text>URL*</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setLinkImg}
+        value={linkImg}
+      />
+
+        <TouchableOpacity onPress={() => Alert.alert('Backward button pressed')}
+        style ={styles.buttonCustom}>
+            <Text style={styles.text}>Add Link</Text>
+        </TouchableOpacity>
 
     </View>
   );
@@ -32,11 +50,34 @@ const styles = StyleSheet.create({
   fixToText: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    marginTop: 30,
   },
-
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 300
+  },
+  buttonCustom: {
+    width: 130 ,height: 40,
+    elevation: 8,
+    backgroundColor: "blue",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    alignItems: "center",
+    margin: 30
+  },
   stretch: {
     width: 300,
     height: 300,
     resizeMode: 'stretch',
   },
+  text:{
+    fontSize:17,
+    color:"white",
+    fontWeight:"bold",
+    textTransform: "uppercase",
+    }   
 });
